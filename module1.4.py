1a031d54114c412a9496b1afa166b33ac9b89e6e
El Revile <132904953+deslapath@users.noreply.github.com>
2024-09-01 21:36:34 +0800

    Модуль 1.4
    Практическая работа по уроку "Организация программ и методы строк."

---
+++ module1.4.py
@@ -0,0 +1,9 @@
+my_string =input() #В консоль пишу "Может, некогда служил тушечницей этот камень", нажимаю Enter
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print('Может, некогда служил тушечницей этот камень'.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
+
+
