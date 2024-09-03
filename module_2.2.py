2dcab6ac5dd78fa1912d55c5dd1aaf05b70a9af7
El Revile <132904953+deslapath@users.noreply.github.com>
2024-09-03 22:32:49 +0800

    Module 2.2
    "Условная конструкция. Операторы if, elif, else."

---
+++ module_2.2.py
@@ -0,0 +1,14 @@
+first = input("")
+second = input("")
+third = input("")
+
+if first == second and second == third and first == third:
+    print('3')
+elif first!= second and second == third or first == second and second != third or first == third:
+    print('2')
+else:
+    print('0')
+
+
+
+
