7e11a6ddf7937d0e126e8f76d21dac464b7dd52c
El Revile <132904953+deslapath@users.noreply.github.com>
2024-09-15 15:21:32 +0800

    Module 3_1
    Домашняя работа по уроку "Пространство имён"

--- module_3.1.py
+++ module_3.1.py
@@ -9,18 +9,19 @@ def string_info(string):
     return (len(string), string.upper(), string.lower())
 def is_contains(string, list_to_search):
     count_calls()
-    if string in list_to_search:
+    string_lower = string.lower()
+    list_to_search_lower = [item.lower() for item in list_to_search]
+    if string_lower in list_to_search_lower:
         return True
     else:
         return False
 
 
-
 print(string_info('Galactica'))
 print(string_info('bumerang'))
 print(string_info('Bambaleylo'))
-print(is_contains('Drevo', ['derevnya', 'september', 'food']))
-print(is_contains('Delphin', ['derevnya', 'Delphin', 'food']))
+print(is_contains('September',('ffhh', 'SEPTEMBER')))
+print(is_contains('Doberman', ('hfj', 'DoBeRmAn', 'LIStopad')))
 print(calls)
 
 
