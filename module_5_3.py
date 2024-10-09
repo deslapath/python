c3a3920721790a3e50ea4967213a29a7683920a5
El Revile <132904953+deslapath@users.noreply.github.com>
2024-10-09 14:14:55 +0800

    Module 5_3
    Домашняя работа по уроку "Перегрузка операторов."

---
+++ module_5_3.py
@@ -0,0 +1,76 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        self.go_to = new_floor
+        if 1 < new_floor < self.number_of_floors:
+            for new_floor in range(1, new_floor + 1):
+                print(new_floor)
+        else:
+            print('Такого этажа не существует')
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'ЖК: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors == other
+
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        return self.__eq__(other) or self.__lt__(other)
+
+    def __gt__(self, other):
+        return not self.__le__(other)
+
+    def __ge__(self, other):
+        return not self.__lt__(other)
+
+    def __add__(self, other):
+        if isinstance(other, House):
+            self.number_of_floors += other.number_of_floors
+        elif isinstance(other, int):
+            self.number_of_floors += other
+        return self
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __iadd__(self, other):
+        return self.__add__(other)
+
+
+h1 = House('Золотой', 14)
+h2 = House('Молодой', 8)
+print(h1)
+print(h2)
+print(h1 == h2)
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+h1 += 10
+print(h1)
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
