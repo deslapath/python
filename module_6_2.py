010ebfac9b7b6d2e00176d4b245522c55d352a2a
El Revile <132904953+deslapath@users.noreply.github.com>
2024-10-22 15:42:11 +0800

    Module 6_2
    Домашнее задание по теме "Доступ к свойствам родителя. Переопределение свойств."

--- module_6_2.py
+++ module_6_2.py
@@ -1,47 +1,44 @@
 class Vehicle:
-
-    __COLOR_VARIANTS = ['red', 'blue', 'black']
-
+    __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
     def __init__(self, owner, __model, __engine_power, __color):
-        self.owner = str(name)
-        self.__model = str(model)
-        self.__engine_power = int(engine_power)
-        self.__color = str(color)
-
+        self.owner = str(owner)
+        self.__model = str (__model)
+        self.__engine_power = int(__engine_power)
+        self.__color = str(__color)
 
     def get_model(self):
-        return f'Модель: {model}'
+        print(f'Модель: {self.__model}')
 
-    def get_horsepower(engine_power):
-        return f'Мощность двигателя: {engine_power}'
+    def get_horsepower(self):
+        print(f'Мощность двигателя: {self.__engine_power}')
 
-    def get_color(color):
-        return f'Цвет: {color}'
+    def get_color(self):
+        print(f'Цвет: {self.__color.lower()}')
 
-    def print_info():
-        return f'{model}, {engine_power}, {color}, Владелец: {name}'
+    def print_info(self):
+        self.get_model()
+        self.get_horsepower()
+        self.get_color()
+        print(f'Владелец: {self.owner}\n')
 
     def set_color(self, new_color):
-        if new_color in set:
-         color = new_color
-
+        reg_colours = [colour.lower() for colour in self.__COLOR_VARIANTS]
+        if new_color.lower() in reg_colours:
+            self.__color = new_color
         else:
-            return f'Нельзя сменить цвет на {new_color}'
+            print(f'Нельзя сменить цвет на {new_color}\n')
 
 class Sedan(Vehicle):
+    __PASSENGERS_LIMIT = 5
 
 
+vehicle1 = Sedan('Fedos', 'Toyota Mark II', 55, 'blue')
 
-# Текущие цвета __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
-Vehicle = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
+vehicle1.print_info()
 
-# Изначальные свойства
-Vehicle.print_info()
+vehicle1.set_color('Pink')
+vehicle1.set_color('BLACK')
+vehicle1.owner = 'Vasyok'
 
-# Меняем свойства (в т.ч. вызывая методы)
-Vehicle.set_color('Pink')
-Vehicle.set_color('BLACK')
-Vehicle.owner = 'Vasyok'
 
-# Проверяем что поменялось
 vehicle1.print_info()
