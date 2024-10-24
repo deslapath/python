b9bd904a03d0e8320e0226343ff4672d92299ece
El Revile <132904953+deslapath@users.noreply.github.com>
2024-10-24 22:31:47 +0800

    module_6_3
    Домашнее задание по теме "Множественное наследование"

--- module_6_3.py
+++ module_6_3.py
@@ -1,30 +1,29 @@
 class Horse:
-    x_distance = 0
-    _sound = 'Frrr'
+    def __init__(self):
+        self.x_distance = 0
+        self.sound = 'Frrr'
+        super().__init__()
 
     def run(self, dx):
         self.x_distance += dx
 
 class Eagle:
-    y_distance = 0
-    sound = 'I train, eat, sleep, and repeat'
+    def __init__(self):
+        self.y_distance = 0
+        self.sound = 'I train, eat, sleep, and repeat'
 
     def fly(self, dy):
         self.y_distance += dy
 
 class Pegasus(Horse, Eagle):
 
-    def __init__(self):
-         self.sound = super()._sound
-         self.sound = super().sound
-
-    def move(self, dx, dy):   #
+    def move(self, dx, dy):
         self.run(dx)
         self.fly(dy)
 
     def get_pos(self):
-         pos_pegasus = (self.x_distance, self.y_distance)
-         return pos_pegasus
+        pos_pegasus = (self.x_distance, self.y_distance)
+        return pos_pegasus
 
     def voice(self):
         print(self.sound)
